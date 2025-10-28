729d4c0ebdc0fe328818db3d3bb253ec5ac3cffe
hermonj-spec <hermon.samsonj@gmail.com>
2025-10-28 18:56:33 +0300

    Commit 1
    A new AccessLecturer with a main method that creates three Lecturer objects with different names and departments was added.

---
+++ Task3/src/AccessLecturer.java
@@ -0,0 +1,9 @@
+import University.Lecturer;
+
+public class AccessLecturer {
+    public static void main(String[] args) {
+        Lecturer l1 = new Lecturer("Norman", "CS");
+        Lecturer l2 = new Lecturer("Lilyan", "IT");
+        Lecturer l3 = new Lecturer("Daniel", "SE");
+    }
+}
